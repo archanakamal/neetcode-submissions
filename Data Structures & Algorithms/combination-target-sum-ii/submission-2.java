026aede0cd4e81efd5e16b48809a687c456287e3
ARCHANA .K <153907688+archanakamal@users.noreply.github.com>
2026-09-06 10:16:41 +0530

    Add: combination-target-sum-ii - submission-2

---
+++ Data Structures & Algorithms/combination-target-sum-ii/submission-2.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static List<List<Integer>> func(int[] nums,int target,int index,List<List<Integer>> res){
+        Arrays.sort(nums);
+        backtrack(nums,target,index,res,0,new ArrayList<>());
+        return res;
+    }
+    public static void backtrack(int nums[],int target,int index,List<List<Integer>> res,int sum,List<Integer> temp){
+       if(sum==target){
+           res.add(new ArrayList(temp));
+           return;
+       }
+       if(sum>target){
+           return;
+       }
+       for(int i=index;i<nums.length;i++){
+           if(i>index && nums[i]==nums[i-1]){
+               continue;
+           }
+           temp.add(nums[i]);
+           backtrack(nums,target,i+1,res,sum+nums[i],temp);
+           temp.remove(temp.size()-1);
+       }
+    }
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        List<List<Integer>> res=new ArrayList<>();
+        res=func(nums,target,0,res);
+        return res;
+    }
+}
